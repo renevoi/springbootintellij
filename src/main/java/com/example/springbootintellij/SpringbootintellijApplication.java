c083b41ccffa019916dc112b8e47544dff04df26
rene <rene.catapang@gmail.com>
2018-10-18 14:48:38 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootintellij/SpringbootintellijApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootintellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootintellijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootintellijApplication.class, args);
+	}
+}
