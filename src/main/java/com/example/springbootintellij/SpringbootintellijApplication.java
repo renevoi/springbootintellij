8201f91317cb8a0a858ae5539197210a90f1df75
rene <rene.catapang@gmail.com>
2018-10-18 15:20:48 +0800

    Put a @Repository on main class

--- src/main/java/com/example/springbootintellij/SpringbootintellijApplication.java
+++ src/main/java/com/example/springbootintellij/SpringbootintellijApplication.java
@@ -2,11 +2,19 @@ package com.example.springbootintellij;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController("/indexpage")
 @SpringBootApplication
 public class SpringbootintellijApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootintellijApplication.class, args);
+	}
+
+	@GetMapping()
+	public String index(){
+		return "Index Page";
 	}
 }
